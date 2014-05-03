d075d19a6b054a261209f8521936d040b1fed201
Daniel Willmann <daniel@totalueberwachung.de>
2014-05-03 03:41:59 +0200

    Pack data structs

--- src/common.h
+++ src/common.h
@@ -3,11 +3,11 @@
 struct pixel {
 	int x, y;
 	uint8_t bright;
-};
+} __attribute__((packed));
 
 struct packet {
 	uint8_t cmd;
 	union {
 		struct pixel pixel;
 	};
-};
+} __attribute__((packed));
