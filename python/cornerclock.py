7f53a793495b22f6d032e2550db5ab14ba0a4834
Roland Hieber <rohieb@rohieb.name>
2014-06-17 01:39:28 +0200

    add python/cornerclock.py

---
+++ python/cornerclock.py
@@ -0,0 +1,11 @@
+#/usr/bin/env python
+
+import datetime
+import time
+import client
+
+while True:
+    t = datetime.datetime.now().strftime(" %H:%M:%S")
+    client.write(client.NUM_SEG_X - len(t), 0, t);
+    time.sleep(1)
+
