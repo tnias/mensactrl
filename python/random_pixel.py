87612b5f815d9366f2bbb3d9bb743254e0bcecd1
Daniel Willmann <daniel@totalueberwachung.de>
2014-10-26 12:15:36 +0100

    python: Support brightness levels in random_pixel and draw blocks

--- python/random_pixel.py
+++ python/random_pixel.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python2
 import client
 import random
+import time
+
+blockx = 5
+blocky = 7
 
 if __name__=="__main__":
     while(True):
-        x = random.random() * client.WIDTH
-        y = random.random() * client.HEIGHT
-        client.set_pixel(x,y, 1 if random.random() > 0.5 else 0)
+        x = random.random() * (client.WIDTH/blockx)
+        y = random.random() * (client.HEIGHT/blocky)
+	x = int(x) * blockx
+	y = int(y) * blocky
+	if random.random() > 0.5:
+            bright = random.random()
+	    bright = (bright * bright) * 255
+	    pix = [int(bright)] * blockx * blocky
+	    client.blit(x, y, blockx, blocky, pix)
+	else:
+	    pix = [0] * blockx * blocky
+	    client.blit(x, y, blockx, blocky, pix)
 
