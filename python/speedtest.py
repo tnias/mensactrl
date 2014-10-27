24f3574395d5b06c15a5c2356e78e7ad782800f8
Roland Hieber <rohieb@rohieb.name>
2014-10-27 22:41:29 +0100

    python: fix color in speedtest

--- python/speedtest.py
+++ python/speedtest.py
@@ -5,4 +5,4 @@ import random
 if __name__=="__main__":
     for y in range(0, client.HEIGHT):
         for x in range(0, client.WIDTH):
-            client.set_pixel(x,y,1)
+            client.set_pixel(x,y,255)
