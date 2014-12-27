9fb2ff92269dd82f012eccad19b3c2d3e3532f76
Emantor <phoenix@emantor.de>
2014-12-27 12:56:41 +0100

    Now with more time awareness

--- python/31c3.py
+++ python/31c3.py
@@ -5,12 +5,29 @@ import time
 import client
 import urllib2
 import json
+import dateutil.parser
 
-greeting = "Stratum 0 Mensadisplay"
+def get_current_talk(talks):
+  for talk in talks:
+    parsed_date = dateutil.parser.parse(talk['date']).replace(tzinfo=None)
+    if(now < parsed_date):
+      saal1_talk = talk
+      return saal1_talk
+      break
+
+greeting = "Stratum 0 Mensadisplay, next talks:"
 tick = 0
 
 j = urllib2.urlopen('https://events.ccc.de/congress/2014/Fahrplan/schedule.json')
 schedule = json.load(j)
+tick = 0
+now = datetime.datetime.now()
+i = 0 
+saal1_talk = get_current_talk(schedule['schedule']['conference']['days'][0]['rooms']['Saal 1'])
+saal2_talk = get_current_talk(schedule['schedule']['conference']['days'][0]['rooms']['Saal 2'])
+saalG_talk = get_current_talk(schedule['schedule']['conference']['days'][0]['rooms']['Saal G'])
+saal6_talk = get_current_talk(schedule['schedule']['conference']['days'][0]['rooms']['Saal 6'])
+
 
 while True:
     t = datetime.datetime.now().strftime(" %H:%M:%S")
@@ -18,13 +35,16 @@ while True:
       j = urllib2.urlopen('https://events.ccc.de/congress/2014/Fahrplan/schedule.json')
       schedule = json.load(j)
       tick = 0
+      now = datetime.datetime.now()
+      i = 0 
+      saal1_talk = get_current_talk(schedule['schedule']['conference']['days'][0]['rooms']['Saal 1'])
+      saal2_talk = get_current_talk(schedule['schedule']['conference']['days'][0]['rooms']['Saal 2'])
+      saalG_talk = get_current_talk(schedule['schedule']['conference']['days'][0]['rooms']['Saal G'])
+      saal6_talk = get_current_talk(schedule['schedule']['conference']['days'][0]['rooms']['Saal 6'])
+
     client.write(0,0,greeting)
     client.write(client.NUM_SEG_X - len(t), 0, t);
     client.write(0,1,"Fahrplan Version:" + schedule['schedule']['version'])
-    saal1_talk = schedule['schedule']['conference']['days'][0]['rooms']['Saal 1'][0]
-    saal2_talk = schedule['schedule']['conference']['days'][0]['rooms']['Saal 2'][0]
-    saalG_talk = schedule['schedule']['conference']['days'][0]['rooms']['Saal G'][0]
-    saal6_talk = schedule['schedule']['conference']['days'][0]['rooms']['Saal 6'][0]
     client.write(0,2,"Saal 1 => " + saal1_talk['start'] + " : " + saal1_talk['title'])
     client.write(0,3,"Saal 2 => " + saal2_talk['start'] + " : " + saal2_talk['title'])
     client.write(0,4,"Saal G => " + saalG_talk['start'] + " : " + saalG_talk['title'])
